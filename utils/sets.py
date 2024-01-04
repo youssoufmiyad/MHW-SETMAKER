e9ec10f8cdd57f9f010c9cec0e6e642e1137b4d6
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-01-04 16:50:16 +0100

    ajout arme dans le set

--- utils/sets.py
+++ utils/sets.py
@@ -1,14 +1,26 @@
 import os
 import json
 from models.armorModel import *
+from models.weaponsModel import *
 
+
+class Set:
+    def __init__(self, armor, weapon):
+        self.armor = armor
+        self.weapon = weapon
 
 # Chargement de la sauvegarde
 
+
 def setsLoading(data):
     sets = []
-    
+
     for d in data["sets"]:
-        sets.append(Armor(d["armor"]["id"],d["armor"]["name"],d["armor"]["rank"],d["armor"]["pieces"],d["armor"]["bonus"]))
+        sets.append(
+            Set(
+                Armor(d["armor"]["id"], d["armor"]["name"], d["armor"]["rank"], d["armor"]["pieces"], d["armor"]["bonus"]),
+                Weapon(d["weapon"]["id"], d["weapon"]["name"], d["weapon"]["type"], d["weapon"]["rarity"], d["weapon"]["attack"], d["weapon"]["elements"], d["weapon"]["crafting"], d["weapon"]["assets"], d["weapon"]["damageType"], d["weapon"]["attributes"])
+            )
+        )
 
     return sets
